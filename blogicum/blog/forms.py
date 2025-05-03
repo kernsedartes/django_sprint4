e5bffe294edbb279c9f5c793903683f2831cafc3
Притчин Андрей <sedartes@mail.ru>
2025-05-03 22:45:58 +0300

    editing

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -28,14 +28,6 @@ class CommentForm(forms.ModelForm):
             'text': forms.Textarea(attrs={'rows': 3}),
         }
 
-    def clean_pub_date(self):
-        pub_date = self.cleaned_data.get('pub_date')
-        if pub_date and pub_date < timezone.now():
-            raise forms.ValidationError(
-                'Дата публикации не может быть более чем на 1 день в прошлом'
-            )
-        return pub_date
-
 
 class EditProfileForm(forms.ModelForm):
     class Meta:
